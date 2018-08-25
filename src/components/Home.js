92457d1b3d52627ea548b7d4e56370828d374470
vcc1432 <valerie.gersen@gmail.com>
2018-08-25 21:46:00 +0200

    Fixed typos

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,12 @@ export default function Home() {
 
     <h3>Rules:</h3>
       <ul>
-        <li>the computer selects a random word</li>
+        <li>The computer selects a random word</li>
         <li>You have to guess the letters of the word</li>
         <li>If you are correct, the letter will be shown</li>
         <li>If you are wrong, you will lose a guess</li>
-        <li> You have a total of <b>six</b> guesses </li>
-        <li> If you have used all your guesses without guessing the correct word, you lose!</li>
+        <li>You have a total of <b>six</b> guesses </li>
+        <li>If you have used all your guesses without guessing the correct word, you lose!</li>
       </ul>
     <h2> Are you ready?</h2>
     <button><Link to={`/hangman`}>Start playing!</Link></button>
